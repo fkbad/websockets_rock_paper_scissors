f74c6cfdee3bdfc7d3c33171b9452e61b98c5ecc
fkbad <fkb@uchicago.edu>
2023-08-02 10:16:17 -0500

    created rocker paper scissors that successfully reports game_state

---
+++ src/chimera/examples/rock_paper_scissors.py
@@ -0,0 +1,271 @@
+# for some reason, importing as chimera.authoring didn't work
+from typing import Dict, List, Optional, Union
+from ..authoring import TwoPlayerGame, Player
+from .. import exceptions as exc
+
+
+class RockPaperScissors(TwoPlayerGame):
+    # for now, not enough involved in status to make it separate
+    # STATUS_GAME_NOT_STARTED = "not-started"
+    # STATUS_WAITING_FOR_MOVES = "waiting-for-moves"
+    # STATUS_PROCESSING_CURRENT_ROUND = "processing-current-round"
+    # STATUS_DONE = "done"
+
+    def __init__(self, game_options=dict()):
+        # game_options is, if not specified, the empty dictionary
+        super().__init__(game_options) 
+
+        # simply a list where [0] is the first players points
+        # can be indexed by player.id because 
+        # id's are assigned in ascending order from 0 as players join
+        self.points: Optional[List[int]] = None
+
+        # move history per round, stored as a list of lists
+        # add a move by doing:
+        # self.history.append([p1_move, p2_move, id of player who won the round (or None) ])
+        self.history: Optional[List[List[Optional[int]]]]= None
+
+        # simply a list where [0] is the first players move this round
+        # can be indexed by player.id because 
+        # id's are assigned in ascending order from 0 as players join
+        self.current_round_moves: Optional[List[Optional[int]]] = None
+
+        # ID of the winning player for the current round, if completed.
+        # None in the case of a Tie, and if the round is in progress
+        self.current_round_winner_id: Optional[int] = None
+
+        # TODO, have this grab from game_options
+        self.points_to_win:int = 3
+
+
+    @property
+    def game_state(self):
+        """
+        returns the game state of the Game
+        should only be called after game has started (.on_start() called) by the server
+
+        Outputs:
+            state dictionary: {
+                "current_round" : {
+                        "moves" : {
+                            "[player_1_name]" : player 1's move,
+                            "[player_2_name]" : player 2's move,
+                            },
+                        "winner" : Optional[Player name] (optional in case of a tie)
+                        },
+                "points" {
+                    "[player_1_name]" : player 1's score,
+                    "[player_2_name]" : player 2's score,
+                    },
+                "history" : [
+                        append-only list of all "completed" rounds. 
+                        where a completed round is one where both
+                        players have given a move.
+                        Format for each entry is the same as the 
+                        "current_round" parameter
+                        ]
+                }
+        """
+        state = {}
+
+        # assert for type checking that the game has started 
+        # and the initial None values are gone
+        assert self.history is not None
+        assert self.points is not None
+        assert self.current_round_moves is not None
+
+        players = [self.get_player_by_id(0),self.get_player_by_id(1)] 
+        player_names = [player.name for player in players]
+
+        # first round
+        current_round = self.get_current_round_for_game_state(player_names = player_names)
+        state["current_round"] = current_round
+
+        # now points
+        points = self.get_points_for_game_state(player_names = player_names)
+        state["points"] = points
+
+        # now history
+        history_list = self.get_history_list_for_game_state(player_names=player_names)
+        state["history"] = history_list
+
+        return state
+
+    @property
+    def done(self) -> bool:
+        # TODO change lmao
+        return False
+
+    @property
+    def winner(self):
+        if self.done:
+            # there must be points at this point
+            assert self.points
+
+            if self.points[0] > self.points[1]:
+                return self.get_player_by_id(0)
+            elif self.points[0] < self.points[1]:
+                return self.get_player_by_id(1)
+            else:
+                return None
+        else:
+            return None
+
+        
+    def on_start(self):
+        self.points = [0] * self.max_players
+        self.history = []
+        self.current_round_moves = [None,None]
+
+        # don't actually notify update since that 
+        # is currently designed to only happen when 
+        # one round is resolved 
+        # self.notify_update()
+
+    def on_end(self):
+        pass
+
+
+    def get_current_round_for_game_state(self,player_names: List[str]) -> Dict[str,Union[Dict[str,str], str]]:
+        """
+        return the properly formatted current_round dictionary
+        to be used in game_state
+
+        "current_round" : {
+                "moves" : {
+                    "[player_1_name]" : player 1's move,
+                    "[player_2_name]" : player 2's move,
+                    },
+                "winner" : Optional[Player name] (optional in case of a tie)
+                },
+        """
+
+        assert self.history is not None
+        assert self.points is not None
+        assert self.current_round_moves is not None
+
+        current_round = {}
+
+        # construct move dict first
+
+        # if the round is in progress, this will then report None
+        # as the moves haven't happened yet
+        move_dict = {player_name:player_move for player_name,player_move in zip(player_names,self.current_round_moves)}
+
+        current_round["moves"] = move_dict
+
+        # next get the winner
+
+        # by default, current winner is None
+        current_round_winner = None
+        if self.current_round_winner_id is not None:
+            # grab the name of the current round's winner
+            current_round_winner = player_names[self.current_round_winner_id]
+
+        # regardless of if the winner is None, add it to the current_round dict
+        current_round["winner"] = current_round_winner
+
+        return current_round
+
+    def get_points_for_game_state(self,player_names: List[str]) -> Dict[str,int]:
+        """
+        return points dictionary for the purposes of reporting game state
+
+        "points" {
+            "[player_1_name]" : player 1's score,
+            "[player_2_name]" : player 2's score,
+            },
+        """
+        assert self.points is not None
+
+        points = {player_name: player_score for player_name,player_score in zip(player_names,self.points)}
+
+        return points
+
+
+
+    def get_history_list_for_game_state(self,player_names: List[str]) -> List[Dict[str,Union[Dict[str,str], str]]]:
+        """
+        return the properly formatted list of rounds for the purposes of reporting
+        game state.
+
+        history[0] is the 1st played round
+        history[-1] is the most recently completed round, where a round has the below format
+
+        "round" : {
+                "moves" : {
+                    "[player_1_name]" : player 1's move,
+                    "[player_2_name]" : player 2's move,
+                    },
+                "winner" : Optional[Player name] (optional in case of a tie)
+                },
+        """
+        # self.history is of the format: 
+        # [p1_move, p2_move, id of player who won the round (or None)]
+
+        history = []
+
+        # assert len(history[0]) == 3
+        assert self.history is not None
+
+        p1_name, p2_name = player_names
+        for p1_move,p2_move,optional_winner_id in self.history:
+            round = {}
+            moves = {p1_name:p1_move, 
+                     p2_name:p2_move}
+
+            winner_name = None
+            if optional_winner_id is not None:
+                winner_name = player_names[optional_winner_id]
+
+            round["moves"] = moves
+            round["winner"] = winner_name
+
+            history.append(round)
+
+        return history
+
+
+
+    def action_move(self,player: Player,data: str):
+        """
+        method called when a player sends a "move" game-action request 
+        to the server
+
+        specifically, they sent a request message for this match 
+        with the message["params"] containing the "action":"move"
+
+        Inputs:
+            player: Player object corresponding to the player making a move
+
+            data: the "data" field of the params. 
+                    I specify this to be either "Rock", "Paper", or "Scissors"
+
+                    considering adding another field for an incompleted move or disconnect
+
+        Outputs:
+            result parameter of the response message to be sent out to 
+            all players, only to be done on a successfully done game-action
+
+            otherwise, raises an exception
+        """
+        if self.done:
+            raise exc.GameNotPlayerTurn
+
+        player_choice = data
+
+        player_id = player.id
+
+
+        # check if player can make a move right now
+        # history is only set to an empty list once the game has started (.on_start() called)
+        # if the player hasn't made a move, update the current moves
+
+        # if the player has already made a move this round, let them update it 
+        # so long as the other player hasn't already made a move
+
+        # check if the round is over, if so, update points
+
+        # check if the game is over, notifying thusly
+
+
---
+++ src/chimera/exceptions.py
@@ -0,0 +1,214 @@
+import json
+
+from enum import Enum
+
+
+class ErrorCode(Enum):
+    # General error codes
+    PARSE_ERROR = -32700
+    INCORRECT_REQUEST = -32600
+    NO_SUCH_OPERATION = -32601
+    INCORRECT_PARAMS = -32602
+
+    # Operation-specific codes
+    UNKNOWN_GAME = -40100
+    ALREADY_IN_MATCH = -40101
+    UNKNOWN_MATCH = -40102
+    DUPLICATE_PLAYER = -40103
+    INCORRECT_MATCH = -40104
+
+    # game-action codes
+    GAME_NOT_PLAYER_TURN = -50100
+    GAME_NO_SUCH_ACTION = -50101
+    GAME_INCORRECT_ACTION_DATA = -50102
+    GAME_INCORRECT_MOVE = -50103
+
+    def __str__(self):
+        return ERROR_MESSAGES[self.value]
+
+
+ERROR_MESSAGES = {
+    # General error codes
+    ErrorCode.PARSE_ERROR.value: "Parse error",
+    ErrorCode.INCORRECT_REQUEST.value: "Incorrect request",
+    ErrorCode.NO_SUCH_OPERATION.value: "No such operation",
+    ErrorCode.INCORRECT_PARAMS.value: "Incorrect parameters",
+
+    # Operation-specific codes
+    ErrorCode.UNKNOWN_GAME.value: "Unknown game",
+    ErrorCode.ALREADY_IN_MATCH.value: "Already in a match",
+    ErrorCode.UNKNOWN_MATCH.value: "Unknown match",
+    ErrorCode.DUPLICATE_PLAYER.value: "Duplicate player name",
+    ErrorCode.INCORRECT_MATCH.value: "Incorrect match",
+
+    # game-action codes
+    ErrorCode.GAME_NOT_PLAYER_TURN.value: "Action not allowed outside player's turn",
+    ErrorCode.GAME_NO_SUCH_ACTION.value: "Unsupported action in game",
+    ErrorCode.GAME_INCORRECT_ACTION_DATA.value: "Incorrect data in game action",
+    ErrorCode.GAME_INCORRECT_MOVE.value: "Incorrect move"
+}
+
+
+
+class ChimeraGameException(Exception):
+
+    def __init__(self, details=None):
+        self.details = details
+
+
+class NotPlayerTurn(ChimeraGameException):
+
+    def __init__(self, details=None):
+        if details is None:
+            details = "It is not your turn."
+
+        super().__init__(details)
+
+
+class IncorrectActionData(ChimeraGameException):
+
+    def __init__(self, details=None):
+        if details is None:
+            details = "Incorrect action data"
+
+        super().__init__(details)
+
+
+class IncorrectMove(ChimeraGameException):
+
+    def __init__(self, details=None):
+        if details is None:
+            details = "Incorrect move"
+
+        super().__init__(details)
+
+
+class ChimeraClientException(Exception):
+    """
+    Base class for all client API exceptions
+    """
+
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class ChimeraConnectionRefusedException(ChimeraClientException):
+    """
+    Raised when the client API is unable to connect to the Chimera server
+    """
+
+    def __init__(self, cre):
+        self.cre_exc = cre
+        super().__init__(str(cre))
+
+
+class MalformedResponse(ChimeraClientException):
+    """
+    Raised when the client API receives a malformed response from the
+    Chimera server
+    """
+
+    def __init__(self, message, response):
+        response_json = json.dumps(response, indent=2)
+        exc_message = f"Malformed response: {message}\n\n{response_json}"
+        super().__init__(exc_message)
+        self._response = response
+
+
+class ErrorResponse(ChimeraClientException):
+    """
+    Base class for exceptions that stem from an error response
+    from the server
+    """
+
+    def __init__(self, code, message, data):
+        self._code = code
+        self._error_message = message
+        self._data = data
+
+        exc_message = f"Error {code}: {message}"
+        if "details" in data:
+            exc_message = f"{exc_message} ({data['details']})"
+        super().__init__(exc_message)
+
+    @property
+    def code(self):
+        return self._code
+
+    @property
+    def error_message(self):
+        return self._error_message
+
+    @property
+    def details(self):
+        return self._data.get("details")
+
+
+class AlreadyInAMatch(ErrorResponse):
+    """
+    Raised when a player tries to join a match, but they are already in a match.
+    """
+    pass
+
+
+class UnknownMatch(ErrorResponse):
+    """
+    Raised when a player provides an incorrect match identifier
+    """
+    pass
+
+
+class DuplicatePlayer(ErrorResponse):
+    """
+    Raised when a player tries to join a match, but there is already
+    a player with the same name in that match.
+    """
+    pass
+
+
+class GameNoSuchAction(ErrorResponse):
+    """
+    Raised when a game action is sent to a match, but the game does
+    not support that action.
+    """
+    pass
+
+
+class GameIncorrectActionData(ErrorResponse):
+    """
+    Raised when a game action is sent to a match, but the data
+    included in the action is incorrect.
+    """
+    pass
+
+
+class GameNotPlayerTurn(ErrorResponse):
+    """
+    Raised when a player performs an action that can only be
+    performed when it is the player's turn.
+    """
+    def __init__(self):
+        code = ErrorCode.GAME_NOT_PLAYER_TURN
+        
+        # because of how dunder str method is implemented in ErrorCode
+        message = str(code)
+
+        # TODO update this after talking with borja
+        data = "not sure what to put here"
+        super().__init__(code, message, data)
+
+
+class GameIncorrectMove(ErrorResponse):
+    pass
+
+
+ERROR_EXCEPTIONS = {
+    ErrorCode.ALREADY_IN_MATCH.value: AlreadyInAMatch,
+    ErrorCode.UNKNOWN_MATCH.value: UnknownMatch,
+    ErrorCode.DUPLICATE_PLAYER.value: DuplicatePlayer,
+    ErrorCode.GAME_NO_SUCH_ACTION.value: GameNoSuchAction,
+    ErrorCode.GAME_INCORRECT_ACTION_DATA.value: GameIncorrectActionData,
+    ErrorCode.GAME_NOT_PLAYER_TURN.value: GameNotPlayerTurn,
+    ErrorCode.GAME_INCORRECT_MOVE.value: GameIncorrectMove
+}
+
